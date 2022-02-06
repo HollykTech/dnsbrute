4c28d537e9c0ed4196afb37d6f87c7bfd26e0411
1kTech <kaostech.naru@gmail.com>
2022-02-06 19:45:01 -0300

    teste123

--- dnsbrute.py
+++ dnsbrute.py
@@ -1,12 +1,4 @@
 import dns.resolver
-import time
-
-def progress_bar(done):
-	print("Carregando: [{0:50s}] {1:.1f}$".format('~' * int(done*50), done*100))
-
-for n in range(101):
-	progress_bar(n/100)
-	time.sleep(1-n)
 
 try:
 	Rs=dns.resolver.query('bancocn.com', 'A')
