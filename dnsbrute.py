3f9b02da194b8b0df26788c598974c26a7053628
1kTech <kaostech.naru@gmail.com>
2022-02-10 03:02:40 -0300

    dnsbrute.py 1.0

--- dnsbrute.py
+++ dnsbrute.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import dns.resolver
-
-try:
-	Rs=dns.resolver.query('bancocn.com', 'A')
-	for R in Rs:
-		print("bancocn.com", R)
-except Exception as erro:
-	print(erro)
-=======
 #!/usr/bin/python3
 
 '''
